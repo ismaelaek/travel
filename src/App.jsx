c1bfc81ac3d6361294391cf79fa30d79e3295753
ismaelaek <ismael.aitelkamel17@gmail.com>
2024-06-13 15:54:33 +0100

    initialized the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=' text-green-600'>
+        Hello React
+     </h1>
+    </>
+  )
+}
+
+export default App
